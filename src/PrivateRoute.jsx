0eb38b2d7394b6ee08a2243961d8c506f3ad0e02
Mohammad Newton <newtonombese1@gmail.com>
2024-08-22 12:16:30 +0300

    Working on the error fixing on the patients dashboard

--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-const PrivateRoute = ({ element: Component, ...rest }) => {
+const PrivateRoute = ({ children }) => {
   const isAuthenticated = !!localStorage.getItem('accessToken'); // Check if token exists
 
-  return isAuthenticated ? <Component {...rest} /> : <Navigate to="/login" />;
+  return isAuthenticated ? children : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
