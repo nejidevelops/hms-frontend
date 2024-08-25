7690a80114feb2874687fc28935381c3b7d92d26
Mohammad Newton <newtonombese1@gmail.com>
2024-08-25 21:20:01 +0300

    Working on the patients dashboard

--- src/components/PatientDashboard.jsx
+++ src/components/PatientDashboard.jsx
@@ -1,13 +1,15 @@
-// src/components/PatientDashboard.jsx
 import { Outlet } from "react-router-dom";
 import Sidebar from "./patients-dashboard/Sidebar";
 import PatientHeader from "./patients-dashboard/Header";
+
 const PatientDashboard = () => {
   return (
     <div className="flex flex-col h-screen">
       <PatientHeader />
       <div className="flex flex-1">
-        <Sidebar />
+        <div className="w-64"> 
+          <Sidebar />
+        </div>
         <div className="flex-1 p-6">
           <Outlet />
         </div>
